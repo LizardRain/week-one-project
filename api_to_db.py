091d7db3f9eceb03071b70b6af30fbbd4374c7ea
LizardRain <108296970+LizardRain@users.noreply.github.com>
2022-06-29 16:42:34 -0400

    Update api_to_db.py

--- api_to_db.py
+++ api_to_db.py
@@ -3,12 +3,24 @@ import pandas as pd
 from bugsnax import *
 
 bugsnax_dict = get_bugsnax()
+location_dict = get_location()
+grumpus_dict = get_grumpus()
 
 bugsnax_df = pd.DataFrame.from_records(bugsnax_dict, exclude=['location'])
+location_df = pd.DataFrame.from_records(location_dict, exclude=['bugsnax', 'inhabitants'])
+grumpus_df = pd.DataFrame.from_records(grumpus_dict, exclude=['fears', 'hates', 'locations', 'loves'])
 
 engine = db.create_engine('sqlite:///bugsnax_db.db')
 
 bugsnax_df.to_sql('bugsnak', con=engine, if_exists='replace', index=False)
+location_df.to_sql('location', con=engine, if_exists='replace', index=False)
+grumpus_df.to_sql('grumpus', con=engine, if_exists='replace', index=False)
 
-query_result = engine.execute("SELECT * FROM bugsnak").fetchall()
-print(pd.DataFrame(query_result))
+bug_q = engine.execute("SELECT * FROM bugsnak").fetchall()
+print(pd.DataFrame(bug_q))
+
+location_q = engine.execute("SELECT * FROM location").fetchall()
+print(pd.DataFrame(location_q))
+
+grump_q = engine.execute("SELECT * FROM grumpus").fetchall()
+print(pd.DataFrame(grump_q))
