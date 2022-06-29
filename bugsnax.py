66f0a2fd9940c7db4cd09867346c4366caf5e482
Sarah Dance <ffsarcd@gmail.com>
2022-06-29 17:23:47 +0000

    changed like 2 more style things

--- bugsnax.py
+++ bugsnax.py
@@ -4,16 +4,16 @@ url = "https://www.bugsnaxapi.com/api"
 
 
 def get_bugsnax():
-  response = requests.get(url + '/bugsnax')
-  res =  response.json()
-  return res['bugsnax']
+    response = requests.get(url + '/bugsnax')
+    res =  response.json()
+    return res['bugsnax']
 
 
 def get_location():
-  response = requests.get(url + '/locations')
-  return response.json()
+    response = requests.get(url + '/locations')
+    return response.json()
 
 
 def get_grumpus():
-  response = requests.get(url + '/grumpuses')
-  return response.json()
+    response = requests.get(url + '/grumpuses')
+    return response.json()
