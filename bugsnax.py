bc019d4de7dd3d4155f25a3aae8d756a10f6d744
Sarah Dance <ffsarcd@gmail.com>
2022-06-29 16:48:43 +0000

    new code to bugsnax.py

--- bugsnax.py
+++ bugsnax.py
@@ -1,11 +1,16 @@
 import requests
 
-url = "https://www.bugsnaxapi.com/api/bugsnax/"
+url = "https://www.bugsnaxapi.com/api"
 
-response = requests.get(url)
+def get_bugsnax():
+  response = requests.get(url + '/bugsnax')
+  res =  response.json()
+  return res['bugsnax']
 
-res = response.json()
-data = res['bugsnax']
+def get_location():
+  response = requests.get(url + '/locations')
+  return response.json()
 
-for item in data:
-	print(item)
+def get_grumpus():
+  response = requests.get(url + '/grumpuses')
+  return response.json()
