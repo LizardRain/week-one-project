009f6d1052aaa6a4165386143f5293195b2bfce6
LizardRain <108296970+LizardRain@users.noreply.github.com>
2022-06-29 16:52:48 -0400

    Update api_to_db.py

--- api_to_db.py
+++ api_to_db.py
@@ -7,9 +7,10 @@ location_dict = get_location()
 grumpus_dict = get_grumpus()
 
 bugsnax_df = pd.DataFrame.from_records(bugsnax_dict, exclude=['location'])
-location_df = pd.DataFrame.from_records(location_dict, exclude=['bugsnax', 'inhabitants'])
-grumpus_df = pd.DataFrame.from_records(grumpus_dict, exclude=['fears', 'hates', 
-                                                              'locations', 'loves'])
+location_df = pd.DataFrame.from_records(location_dict, 
+                                        exclude=['bugsnax', 'inhabitants'])
+grumpus_df = pd.DataFrame.from_records(grumpus_dict, 
+                                       exclude=['fears', 'hates', 'locations', 'loves'])
 
 engine = db.create_engine('sqlite:///bugsnax_db.db')
 
